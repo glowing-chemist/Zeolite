c7f67a42cf724eae45da0bd10953e0aa2da590ad
Oliver Smith <olie.smit0@gmail.com>
2018-09-08 13:56:51 +0100

    Implement constructors for the three basic emiters.

--- src/Emiter/mod.rs
+++ src/Emiter/mod.rs
@@ -1,4 +1,7 @@
 extern crate glm;
+extern crate rand;
+
+use Emiter::rand::distributions::{IndependentSample, Range};
 
 pub enum ParticleType {
     Sprite(String),
@@ -6,6 +9,8 @@ pub enum ParticleType {
     Point,
 } 
 
+// Struct to describe a particle that will initially be
+// emited from one of the base or custom emiters.
 pub struct Particle {
     mPosition : glm::Vector3<f32>,
     mAcceleration : glm::Vector3<f32>,
@@ -13,6 +18,14 @@ pub struct Particle {
     mType : ParticleType,
 }
 
+pub struct ParticleDescription {
+    mMinSize : u32,
+    mMaxSize : u32,
+    mMaxLifeTime : u32,
+    mType : ParticleType
+}
+
+// Users may add custom structs that implement the emiter trait 
 pub trait Emiter {
     fn Emit(&self) -> Particle;
 }
@@ -22,34 +35,53 @@ pub trait Emiter {
 
 pub struct PlaneEmiter {
     mNormal : glm::Vector3<f32>,
+    mPosition : glm::Vector3<f32>,
     mX : u32,
     mY : u32,
+    mDesc : ParticleDescription,
+    mRng : rand::XorShiftRng,
 }
 
 impl PlaneEmiter {
-    pub fn new(normal : glm::Vector3<f32>, x : u32, y : u32) -> PlaneEmiter {
-        PlaneEmiter{mNormal : normal, mX : x, mY : y}
+    pub fn new( position : glm::Vector3<f32>, 
+                normal : glm::Vector3<f32>, 
+                x : u32, 
+                y : u32, 
+                desc : ParticleDescription) -> PlaneEmiter {
+        PlaneEmiter{mPosition : position, mNormal : normal, mX : x, mY : y, mDesc : desc, mRng : rand::XorShiftRng::new_unseeded()}
     }
 }
 
 pub struct StreamEmiter {
     mDirection : glm::Vector3<f32>,
+    mPosition : glm::Vector3<f32>,
     mAngle : f32,
+    mDesc : ParticleDescription,
+    mRng : rand::XorShiftRng,
 }
 
 impl StreamEmiter {
-    pub fn new(direction : glm::Vector3<f32>, angle : f32) -> StreamEmiter {
-        StreamEmiter{mDirection : direction, mAngle : angle}
+    pub fn new( position : glm::Vector3<f32>, 
+                direction : glm::Vector3<f32>, 
+                angle : f32,
+                desc : ParticleDescription) -> StreamEmiter {
+        StreamEmiter{mPosition : position, mDirection : direction, mAngle : angle, mDesc : desc, mRng : rand::XorShiftRng::new_unseeded()}
     }
 }
 
 pub struct SphereEmiter {
     mCenter : glm::Vector3<f32>,
+    mPosition : glm::Vector3<f32>,
     mRadius : f32,
+    mDesc : ParticleDescription,
+    mRng : rand::XorShiftRng,
 }
 
 impl SphereEmiter {
-    pub fn new(center : glm::Vector3<f32>, radius : f32) -> SphereEmiter {
-        SphereEmiter{mCenter : center, mRadius : radius}
+    pub fn new( position : glm::Vector3<f32>,
+                center : glm::Vector3<f32>, 
+                radius : f32,
+                desc : ParticleDescription) -> SphereEmiter {
+        SphereEmiter{mPosition : position, mCenter : center, mRadius : radius, mDesc : desc, mRng : rand::XorShiftRng::new_unseeded()}
     }
 }
